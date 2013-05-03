8b6782e8fe2cb46fdea60ada6563081640f3c7aa
bradshoow <mat.simonnet@gmail.com>
2013-05-03 06:48:10 +0200

    againagain

---
+++ src/orion/brest/sara/Auto.java
@@ -0,0 +1,31 @@
+//package orion.brest.sara;
+//
+//import android.widget.CheckBox;
+//
+//public class Auto {
+//
+//	private CheckBox speedAutoCheckBox = null;
+//	private CheckBox bearingAutoCheckBox = null;
+//	
+//	
+//	public CheckBox getSpeedAutoCheckBox() {
+//		return speedAutoCheckBox;
+//	}
+//	
+//	public void setSpeedAutoCheckBox(CheckBox speedAutoCheckBox) {
+//		this.speedAutoCheckBox = speedAutoCheckBox;
+//	}
+//
+//	
+//	public CheckBox getBearingAutoCheckBox() {
+//		return bearingAutoCheckBox;
+//	}
+//
+//	public void setBearingAutoCheckBox(CheckBox bearingAutoCheckBox) {
+//		this.bearingAutoCheckBox = bearingAutoCheckBox;
+//	}
+//	
+//	
+//	
+//	
+//}
